631541645251a5813ab9c7de9f6969f97c164322
VICTOR HUGO <victor.oliveira@willbank.com.br>
2022-04-18 20:53:45 -0300

    testing mock http request

--- tests/infra/swapi_api_consumer/test_swapi_api_consumer.py
+++ tests/infra/swapi_api_consumer/test_swapi_api_consumer.py
@@ -2,10 +2,10 @@ from infra.swapi_api_consumer import SwapiApiConsumer
 
 
 class TestSwapiApiConsumer:
-    def test_when_you_do_a_get_and_return_answer(self):
+    def test_when_you_do_a_get_and_return_answer(self, requests_mock):
+        requests_mock.get("https://swapi.dev/api/starships/", status_code=200, json={"victor": "hugo"})
         page: int = 1
 
         req = SwapiApiConsumer()
         response = req.get_starships(page=page)
 
-        print(response)
